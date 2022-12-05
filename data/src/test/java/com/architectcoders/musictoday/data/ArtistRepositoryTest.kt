75b2b01eb3806c73a8512c250420e7a02758ebce
yerih <yerih7tup@gmail.com>
2022-12-05 17:41:05 -0400

    test assert true

---
+++ data/src/test/java/com/architectcoders/musictoday/data/ArtistRepositoryTest.kt
@@ -0,0 +1,11 @@
+package com.architectcoders.musictoday.data
+
+import org.junit.Assert
+import org.junit.Assert.*
+import org.junit.Test
+
+class ArtistRepositoryTest{
+
+    @Test
+    fun `Assert it works`() = assertTrue(true)
+}
