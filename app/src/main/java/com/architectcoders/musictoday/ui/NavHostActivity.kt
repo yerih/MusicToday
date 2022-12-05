25ca47b5dd63e6a2a7d61f745a7f717bc3acd1b1
yerih <yerih7tup@gmail.com>
2022-12-05 13:58:05 -0400

    test implementation libs

--- app/src/main/java/com/architectcoders/musictoday/ui/NavHostActivity.kt
+++ app/src/main/java/com/architectcoders/musictoday/ui/NavHostActivity.kt
@@ -3,15 +3,12 @@ package com.architectcoders.musictoday.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.architectcoders.musictoday.R
-import com.orhanobut.logger.AndroidLogAdapter
-import com.orhanobut.logger.Logger
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class NavHostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Logger.addLogAdapter(AndroidLogAdapter())
         setContentView(R.layout.activity_nav_host)
     }
 }
