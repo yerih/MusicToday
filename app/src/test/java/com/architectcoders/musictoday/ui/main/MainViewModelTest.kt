d380a21577f6f40587fa1c35722471800065a110
yerih <yerih7tup@gmail.com>
2022-12-15 18:25:00 -0400

    viewmodel testing. state is updated by cache

---
+++ app/src/test/java/com/architectcoders/musictoday/ui/main/MainViewModelTest.kt
@@ -0,0 +1,59 @@
+package com.architectcoders.musictoday.ui.main
+
+import app.cash.turbine.test
+import com.architectcoders.musictoday.sampleArtist
+import com.architectcoders.musictoday.ui.main.MainViewModel.*
+import com.architectcoders.musictoday.usecases.GetPopularArtistUseCase
+import com.architectcoders.musictoday.usecases.RequestArtistsUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.whenever
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class MainViewModelTest {
+
+    @Mock
+    lateinit var requestArtistUseCase: RequestArtistsUseCase
+
+    @Mock
+    lateinit var getPopularArtistUseCase: GetPopularArtistUseCase
+
+    private lateinit var vm: MainViewModel
+    private var artists = listOf(sampleArtist.copy(id = 2))
+    private val testDispatcher = StandardTestDispatcher()
+
+
+    @Before
+    fun setUp(){
+        Dispatchers.setMain(testDispatcher)
+        whenever(getPopularArtistUseCase()).thenReturn(flowOf(artists))
+        vm = MainViewModel(getPopularArtistUseCase, requestArtistUseCase)
+    }
+
+    @After
+    fun tearDown(){
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `State is updated with cache`() = runTest {
+        vm.state.test{
+            assertEquals(UiState(), awaitItem())
+            assertEquals(UiState(artists = artists), awaitItem())
+        }
+    }
+}
---
+++ testCommon/src/main/java/com/architectcoders/musictoday/samples.kt
@@ -0,0 +1,14 @@
+package com.architectcoders.musictoday
+
+import com.architectcoders.musictoday.domain.Artist
+
+val sampleArtist = Artist(
+    id = 0,
+    name = "Farruko",
+    biography = "is a music artist",
+    publishingDate = "2022-10-10",
+    imageUrl = "https://e-cdns-images.dzcdn.net/images/artist/61cb20fc86e37eac43834dc6e7484e1b/500x500-000000-80-0-0.jpg",
+)
+
+
+
